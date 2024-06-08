8de4f106760ddf2b823fec2d75f9202038d9b239
jackfromeast <thisiszhengyu@gmail.com>
2024-06-08 14:42:49 -0400

    [ConcolicExecution] Ensure the jalangi2 runtime and analysis will be installed before any scripts on the page.

--- concolic-engine/browser/callbacks/install-analysis-scripts-callbacks.js
+++ concolic-engine/browser/callbacks/install-analysis-scripts-callbacks.js
@@ -6,21 +6,21 @@ const fs = require('fs').promises;
 const path = require('path');
 
 /**
+ * [DEPRECATED]
  * @description
  * --------------------------------
- * The callback function will intakes the visitor and page objects as the argument,
- * register a listener on every new javascript context and install the Jalani2 runtime
+ * The callback function will intake the visitor and page objects as the argument,
+ * register a listener on every new javascript context and install the Jalangi2 runtime
  * and the analysis scripts.
  * 
  * Here we use the Runtime.executionContextCreated event besides the page.evaluateOnNewDocument,
- * because we need it to be executed in the context like sevice workers, etc.
+ * because we need it to be executed in contexts like service workers, etc.
  * 
- * We are injecting the content of the script directly instead of adding a script tag and set the
- * src because we can guarantee that the script will installed before any other script on the page.
+ * We are injecting the content of the script directly instead of adding a script tag and setting the
+ * src because we can guarantee that the script will be installed before any other script on the page.
  * 
  * @TODO
  * --------------------------------
- * 1/ Add the Jalangi2 runtime bundle script to the page, now we still use the proxy server insturmentation
  * 
  * @param {Visitor} visitor 
  * @param {*} page 
@@ -33,7 +33,7 @@ async function installAnalysisScriptsCb(visitor, page){
   visitor.curCDPsession.on('Runtime.executionContextCreated', async (event) => {
     const context = event.context;
     if (context.origin) {
-      // Inject the Jalangi2 runtime and analysis scripts
+      
       const evalString = `
         (function() {
           function injectScripts() {
@@ -49,53 +49,8 @@ async function installAnalysisScriptsCb(visitor, page){
           awaitPromise: true
         });
       } catch(err){
-        console.warn(`[installAnalysisScriptsCb] Injecting scripts failed (due to debug context): ${err}`);
+        visitor.logger.warn(`[installAnalysisScriptsCb] Injecting scripts failed (due to debug context): ${err}`);
       }
     }
   });
 };
-
-// async function installAnalysisScriptsCb(visitor, page){
-//   const analysisScriptPath = path.resolve(visitor.config.others.ANALYSIS_SCRIPT_PATH);
-//   const analysisScriptContent = await fs.readFile(analysisScriptPath, 'utf8');
-
-//   // Inject the Jalangi2 runtime and analysis scripts before any other script
-//   visitor.curCDPsession.on('Runtime.executionContextCreated', async (event) => {
-//     const context = event.context;
-//     if (context.origin) {
-//       // Inject the Jalangi2 runtime and analysis scripts
-//       const evalString = `
-//         (function() {
-//           function injectScripts() {
-//             // const jalangiRuntime = document.createElement('script');
-//             // jalangiRuntime.src = '${visitor.config.others.JALANGI_RUNTIME_PATH}';
-//             const analysisScript = document.createElement('script');
-//             analysisScript.src = '${visitor.config.others.ANALYSIS_SCRIPT_PATH}';
-            
-//             // const firstScript = document.head.getElementsByTagName('script')[0];
-//             // if (firstScript) {
-//             //   // document.head.insertBefore(jalangiRuntime, firstScript);
-//             //   document.head.insertBefore(analysisScript, firstScript);
-//             // } else {
-//             //   // document.head.appendChild(jalangiRuntime);
-//             //   document.head.appendChild(analysisScript);
-//             // }
-            
-//             // After we can inject Jalangi2 runtime, we can inject the analysis script insertAfter the Jalangi2 runtime
-//             document.head.appendChild(analysisScript);
-//           }
-//           if(J$$ && !J$$.analysis) injectScripts();
-//         })();
-//       `;
-//       try{
-//         await visitor.curCDPsession.send('Runtime.evaluate', {
-//           expression: evalString,
-//           contextId: context.id,
-//           awaitPromise: true
-//         });
-//       } catch(err){
-//         console.warn(`[installAnalysisScriptsCb] Injecting scripts failed (due to debug context): ${err}`);
-//       }
-//     }
-//   });
-// };
