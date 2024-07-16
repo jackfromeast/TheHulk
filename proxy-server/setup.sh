1cbcebf5784cabb4f4bc37e7679322a991d8fc59
jackfromeast <thisiszhengyu@gmail.com>
2024-07-16 12:28:26 -0400

    [Misc] Adjust the project folders that following https://github.com/AFLplusplus/Grammar-Mutator, which is looking nicer

--- proxy-server/setup.sh
+++ proxy-server/setup.sh
@@ -4,4 +4,7 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate mitmproxy_env
 
+# Export PYTHONPATH with the relative path
+export PYTHONPATH=$(dirname "$0")/../libs/mitmproxy:$PYTHONPATH
+
 mitmdump --set stream_large_bodies=500m --anticache --quiet -p 8899 -s "proxy.py"
