4e07bfc0773c68cbea640f633740b9cafeadcb21
jackfromeast <thisiszhengyu@gmail.com>
2024-07-18 11:36:36 -0400

    [Concolic] Update the utils by separating the taint related helper functions and non-taint related helper functions

--- concolic-engine/runtime-analysis/src/taint-helper.js
+++ concolic-engine/runtime-analysis/src/taint-helper.js
@@ -1,57 +1,110 @@
 import { TaintValue, WrappedValue } from './values/wrapped-values.js';
-import { TaintInfo } from './values/taint-info.js';
+import { TaintInfo, TaintPropName } from './values/taint-info.js';
+import { Utils } from './utils/util.js';
 
-
+/**
+ * @description
+ * --------------------------------
+ * This class defines the taint-related helper functions
+ * This class help you check, add, merge, and remove taint information
+ */
 export class TaintHelper {
   
-  static rconcrete(value) {
-    if (value instanceof WrappedValue) {
-      return this.concrete(value.getConcrete());
+  /**
+   * Create a new taint value
+   * @param {*} value 
+   * @param {TaintInfo} taintInfo 
+   */
+  static createTaintValue(value, taintInfo) {
+    // Check if value is tainted already
+    if (TaintHelper.isTainted(value)) {
+      // TODO: If it is already tainted, we need to merge the taint info
+      // Currently, we just return the value itself
+      return value;
+    }
+
+    if (Utils.isPrimitive(value)) {
+      return new TaintValue(value, taintInfo);
+    }
+    else {
+      try {
+        Object.defineProperty(value, TaintPropName, {
+          value: taintInfo,
+          enumerable: false,
+          writable: true,
+          configurable: true
+        });
+      }
+      catch (e) {
+        console.log(`Error in adding taint to ${value}: ${e}`);
+      }
+    }
+  }
+
+  static risTainted(value) {
+    if (TaintHelper.isTainted(value)) {
+      return true;
     } else if (Array.isArray(value)) {
-      return value.map(item => this.concrete(item));
+      return value.some(item => TaintHelper.risTainted(item));
+    } else if (value && typeof value === 'object' && value.constructor === Object) {
+      return Object.keys(value).some(key => TaintHelper.risTainted(value[key]));
+    }
+    return false;
+  }
+
+  /**
+   * Check if the value is tainted in one level
+   * @param {*} value 
+   * @returns 
+   */
+  static isTainted(value) {
+    return value instanceof TaintValue || (!Utils.isPrimitive(value) && value[TaintPropName]);
+  }
+
+  /**
+   * Concrete the value if it is tainted recursively
+   * @param {*} value 
+   * @returns 
+   */
+  static rconcrete(value) {
+
+    if (TaintHelper.isTainted(value)) {
+      return TaintHelper.concrete(value.getConcrete());
+    } else if (Array.isArray(value)) {
+      return value.map(item => TaintHelper.rconcrete(item));
     } else if (value && typeof value === 'object' && value.constructor === Object) {
       return Object.keys(value).reduce((acc, key) => {
-        acc[key] = this.concrete(value[key]);
+        acc[key] = TaintHelper.rconcrete(value[key]);
         return acc;
       }, {});
     }
+
     return value;
   }
 
+  /**
+   * Concrete the value if it is tainted in one level
+   * @param {*} value 
+   * @returns 
+   */
   static concrete(value) {
-    if (value instanceof WrappedValue){
+    if (TaintHelper.isTainted(value)) {
       return value.getConcrete();
     }
     return value;
   }
 
-  static isTainted(value) {
-    return value instanceof TaintValue;
+  static getTaintInfo(value) {
+    if (TaintHelper.isTainted(value)) {
+      if (value instanceof TaintValue) {
+        return value.getTaintInfo();
+      } else{
+        return value[TaintPropName];
+      }
+    }
+    return null;
   }
-
-  static reportDangerousFlow(sourceReason, sourceLoc, sinkReason, sinkLoc, taintedValue, iid) {
-    console.log("%c[TheHulk] Found a dangerous flow from %s to %s!",
-                'background: #222; color: #bada55',            
-                sourceReason, sinkReason);
     
-    const clonedTaintedValue = JSON.parse(JSON.stringify(taintedValue));
-
-    J$$.analysis.dangerousFlows.push({
-      sourceReason: sourceReason,
-      sourceLoc: sourceLoc,
-      sinkReason: sinkReason,
-      sinkLoc: sinkLoc,
-      taintedValue: clonedTaintedValue,
-      iid: iid
-    });
-  }
-
-  static reportUnsupportedBuiltin(builtinName, ) {
-    console.log("%c[TheHulk] Unsupported builtin %s!",
-                'background: white; color: brown',            
-                builtinName);
-  }
-
   /**
    * Check if any of the arguments are tainted
    * If function has been called in this way: f.apply(this, args),
@@ -65,7 +118,7 @@ export class TaintHelper {
     if (args.length === 0) { return false; }
 
     let argsArray = args;
-    if (TaintHelper.isArguments(args)) {
+    if (Utils.isArguments(args)) {
       argsArray = Array.from(args);
     }
     
@@ -82,77 +135,4 @@ export class TaintHelper {
 
     return argsArray.some(arg => TaintHelper.isTainted(arg));
   }
-
-  /**
-   * Retrun the array like arguments
-   * 
-   * @param {Argruments} args 
-   * @param {String} reflected 
-   * @returns 
-   */
-  static getArrayLikeArguements(args, reflected) {
-    if (args.length === 0) { return []; }
-      
-    let argsArray = args;
-    if (TaintHelper.isArguments(args)) {
-      argsArray = Array.from(args);
-    }
-    
-    if (reflected === 'apply') {
-      // For f.apply(this, args)
-      // If there is only one argument, it is the arg itself
-      // If there is more than one argument, it is [arg1, arg2, ...]
-      if (argsArray[1] instanceof Array) {
-        return argsArray[1];
-      } else {
-        return [argsArray[1]];
-      }
-    }
-
-    return argsArray;
-  }
-
-
-  static isNativeFunction(f) {
-    const toString = Object.prototype.toString;
-    const fnToString = Function.prototype.toString;
-    const reHostCtor = /^\[object .+?Constructor\]$/;
-    const reNative = RegExp("^" +
-        String(toString)
-            .replace(/[.*+?^${}()|[\]\/\\]/g, "\\$&")
-            .replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, "$1.*?") + "$"
-    );
-
-    function isNativeCore(value) {
-        if (value.hasOwnProperty('toString')) {
-            console.warn('WARNING: isNativeFunction will not work on custom toString methods. We assume nobody would overwrite core method toStrings');
-            return false;
-        }
-
-        if (typeof(value) === "function") {
-            return reNative.test(fnToString.call(value)); 
-        } else if (typeof(value) === "object") {
-            return reHostCtor.test(toString.call(value));
-        } else {
-            return false;
-        }
-    }
-
-    if (f === null || f === undefined) {
-        console.warn('isNativeFunction called on null or undefined');
-        return false;
-    }
-
-    if (typeof(f) === "function" || typeof(f) === "object") {
-        return isNativeCore(f);
-    } else {
-        console.warn('isNativeFunction called on non-function/non-object');
-        return false;
-    }
-  }
-
-  static isArguments(args) {
-    return Object.prototype.toString.call(args) === '[object Arguments]';
-  }
-
 }
---
+++ concolic-engine/runtime-analysis/src/utils/util.js
@@ -0,0 +1,135 @@
+import { TaintHelper } from "../taint-helper.js";
+
+/**
+ * @description
+ * --------------------------------
+ * General utility functions that are used across the analysis
+ */
+export class Utils {
+  static reportDangerousFlow(sourceReason, sourceLoc, sinkReason, sinkLoc, taintedValue, iid) {
+    console.log("%c[TheHulk] Found a dangerous flow from %s to %s!",
+                'background: #222; color: #bada55',            
+                sourceReason, sinkReason);
+    
+    const clonedTaintedValue = JSON.parse(JSON.stringify(taintedValue));
+
+    J$$.analysis.dangerousFlows.push({
+      sourceReason: sourceReason,
+      sourceLoc: sourceLoc,
+      sinkReason: sinkReason,
+      sinkLoc: sinkLoc,
+      taintedValue: clonedTaintedValue,
+      iid: iid
+    });
+  }
+
+  static reportUnsupportedBuiltin(builtinName, ) {
+    console.log("%c[TheHulk] Unsupported builtin %s!",
+                'background: white; color: brown',            
+                builtinName);
+  }
+
+  /**
+   * Retrun the array like arguments
+   * 
+   * @param {Argruments} args 
+   * @param {String} reflected 
+   * @returns 
+   */
+  static getArrayLikeArguements(args, reflected) {
+    if (args.length === 0) { return []; }
+      
+    let argsArray = args;
+    if (Utils.isArguments(args)) {
+      argsArray = Array.from(args);
+    }
+    
+    if (reflected === 'apply') {
+      // For f.apply(this, args)
+      // If there is only one argument, it is the arg itself
+      // If there is more than one argument, it is [arg1, arg2, ...]
+      if (argsArray[1] instanceof Array) {
+        return argsArray[1];
+      } else {
+        return [argsArray[1]];
+      }
+    }
+
+    return argsArray;
+  }
+
+  static isNativeFunction(f) {
+    const toString = Object.prototype.toString;
+    const fnToString = Function.prototype.toString;
+    const reHostCtor = /^\[object .+?Constructor\]$/;
+    const reNative = RegExp("^" +
+        String(toString)
+            .replace(/[.*+?^${}()|[\]\/\\]/g, "\\$&")
+            .replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, "$1.*?") + "$"
+    );
+
+    function isNativeCore(value) {
+        if (value.hasOwnProperty('toString')) {
+            console.warn('WARNING: isNativeFunction will not work on custom toString methods. We assume nobody would overwrite core method toStrings');
+            return false;
+        }
+
+        if (typeof(value) === "function") {
+            return reNative.test(fnToString.call(value)); 
+        } else if (typeof(value) === "object") {
+            return reHostCtor.test(toString.call(value));
+        } else {
+            return false;
+        }
+    }
+
+    if (f === null || f === undefined) {
+        console.warn('isNativeFunction called on null or undefined');
+        return false;
+    }
+
+    if (typeof(f) === "function" || typeof(f) === "object") {
+        return isNativeCore(f);
+    } else {
+        console.warn('isNativeFunction called on non-function/non-object');
+        return false;
+    }
+  }
+
+  static isArguments(args) {
+    return Object.prototype.toString.call(args) === '[object Arguments]';
+  }
+
+  static isPrimitive(value) {
+    return value !== Object(value);
+  }
+
+  /**
+   * Check the real type of the value no matter if it is tainted
+   * @param {*} value 
+   */
+  static realTypeOf(value) {
+    if (TaintHelper.isTainted(value)) {
+      return typeof value.getConcrete();
+    }
+    return typeof value;
+  }
+
+  /**
+   * Check if the value is a string no matter if it is tainted
+   * @param {*} value 
+   * @returns 
+   */
+  static isString(value) {
+    return Utils.realTypeOf(value) === 'string' || TaintHelper.concrete(value) instanceof String;
+  }
+
+  /**
+   * Check if the value is an RegExpStringIterator no matter if it is tainted
+   * @param {*} value 
+   * @returns 
+   */
+  static isRegExpStringIterator(value) {
+    return TaintHelper.concrete(value) instanceof RegExpStringIterator;
+  }
+}
