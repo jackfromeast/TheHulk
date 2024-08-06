7e267581ead2706460746a1e0e16359638c88b68
jackfromeast <thisiszhengyu@gmail.com>
2024-08-06 11:03:09 -0400

    [Concretization] Add the safe version of builtins in case they has been overwritten by user program

--- concolic-engine/runtime-analysis/src/rules/rule-concretize.js
+++ concolic-engine/runtime-analysis/src/rules/rule-concretize.js
@@ -1,7 +1,7 @@
 import { TaintHelper } from "../taint-helper.js";
 
 /**
- * This class contains the list of functions that we know how to concretize its base and args
+ * This class contains the list of functions that we know how to concretize its base and args.
  * 
  * - We don't want to blindly concretize all the base and args for all the unsupported functions 
  *   as it will cause performance issues and lead to taint loss.
@@ -9,11 +9,14 @@ import { TaintHelper } from "../taint-helper.js";
  *   exception and break the program.
  * - Therefore, we need to concretize the base and args for the unsupported functions based on the 
  *   defined instructions. By default, we will concretize the base and args by one level, but for specific
- *   fucntions, we will concretize the base or args in a fine grain manner.
+ *   functions, we will concretize the base or args in a fine-grained manner.
  */
 export class DefinedConcretizeBuiltinHelper {
   constructor() {
     this.prepare();
+    // Incase the Array.prototype.filter is overwritten
+    this.array_prototype_filter = Array.prototype.filter;
+    this.object_entries = Object.entries;
   }
 
   /**
@@ -52,12 +55,15 @@ export class DefinedConcretizeBuiltinHelper {
   concretizedDict = {
     // Browser APIs with specific concretization strategies
     "FinalizationRegistry.prototype.register": [FinalizationRegistry.prototype.register, "NONE"],
+    "Performance.prototype.measure": [Performance.prototype.measure, "ARG1_OBJECT_RECURSIVE && ARGS_SELF"],
+    "IDBObjectStore.prototype.get": [IDBObjectStore.prototype.get, "ARG0_ANY_RECURSIVE && ARGS_SELF"],
+    "IDBObjectStore.prototype.put": [IDBObjectStore.prototype.put, "ARG0_ANY_RECURSIVE && ARGS_SELF"],
   };
 
   prepare() {
     // Check if PresentationRequest is defined
     if (typeof PresentationRequest !== 'undefined') {
-      this.concretizedDict["PresentationRequest"] = [PresentationRequest, "ARG0_ARRAY_ONE_LEVEL"];
+      this.concretizedDict["PresentationRequest"] = [PresentationRequest, "ARG0_ARRAY_ONE_LEVEL || ARGS_SELF"];
     }
   }    
 
@@ -72,7 +78,7 @@ export class DefinedConcretizeBuiltinHelper {
    */
   validateStrategyString(strategyString) {
     const validStrategies = [
-      'NONE', 'ARGS_SELF', 'BASE_SELF', 'ARG0_ARRAY_ONE_LEVEL', 'BASE_AND_ARGS_SELF'
+      'NONE', 'ARGS_SELF', 'BASE_SELF', 'ARG0_ARRAY_ONE_LEVEL', 'BASE_AND_ARGS_SELF', 'ARG1_OBJECT_RECURSIVE', 'ARG0_ANY_RECURSIVE'
     ];
     const strategyTokens = strategyString.split(/\s*(&&|\|\|)\s*/);
     for (const token of strategyTokens) {
@@ -92,43 +98,72 @@ export class DefinedConcretizeBuiltinHelper {
 
     const strategies = {
       'NONE': (base, args) => {
-        return [base, args];
+        return [base, args, true];
       },
       'ARGS_SELF': (base, args) => {
         args = Array.from(args).map(arg => TaintHelper.concreteHard(arg));
-        return [base, args];
+        return [base, args, true];
       },
       'BASE_SELF': (base, args) => {
         base = TaintHelper.concreteHard(base);
-        return [base, args];
+        return [base, args, true];
       },
       'ARG0_ARRAY_ONE_LEVEL': (base, args) => {
         if (Array.isArray(args[0])) {
           args[0] = args[0].map(item => TaintHelper.concreteHard(item));
-        }else {
-          args = Array.from(args).map(arg => TaintHelper.concreteHard(arg));
+          return [base, args, true];
         }
-        return [base, args];
+        return [base, args, false];
+      },
+      'ARG1_OBJECT_RECURSIVE': (base, args) => {
+        if (typeof args[1] === 'object') {
+          args[1] = TaintHelper.rconcreteHard(args[1]);
+          return [base, args, true];
+        }
+        return [base, args, false];
+      },
+      'ARG0_ANY_RECURSIVE': (base, args) => {
+        args[0] = TaintHelper.rconcreteHard(args[0]);
+        return [base, args, true];    
       },
       'BASE_AND_ARGS_SELF': (base, args) => {
         base = TaintHelper.concreteHard(base);
         args = Array.from(args).map(arg => TaintHelper.concreteHard(arg));
-        return [base, args];
+        return [base, args, true];
       }
     };
 
-    let modifiedStrategyString = strategyString;
+    const applyStrategy = (strategy, base, args) => strategies[strategy](base, args);
 
-    Object.keys(strategies).forEach(key => {
-      const regex = new RegExp(`\\b${key}\\b`, 'g');
-      modifiedStrategyString = modifiedStrategyString.replace(regex, `strategies.${key}`);
-    });
+    // Parse the strategyString and build a function that applies it
+    const strategyFunction = (base, args) => {
+      const tokens = this.array_prototype_filter.call(strategyString.split(/\s*(&&|\|\|)\s*/), Boolean);
+      let result = [base, args, false];
+      let skipNext = false;
 
-    const strategyFunction = new Function('strategies', 'base', 'args', `
-      return ${modifiedStrategyString}(base, args);
-    `);
+      for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i].trim();
 
-    return strategyFunction.bind(null, strategies);
+        if (skipNext) {
+          skipNext = false;
+          continue;
+        }
+
+        if (strategies[token]) {
+          const [newBase, newArgs, changed] = applyStrategy(token, ...result);
+          result = [newBase, newArgs, changed || result[2]];
+
+          // Handle OR condition: skip next strategy if this one succeeded
+          if (tokens[i + 1] === '||' && changed) {
+            skipNext = true;
+          }
+        }
+      }
+
+      return result;
+    };
+
+    return strategyFunction;
   }
 
   /**
@@ -142,7 +177,7 @@ export class DefinedConcretizeBuiltinHelper {
     let concretizationStrategy = "ARGS_SELF";
 
     // Determine the concretization strategy for the function
-    for (const [key, value] of Object.entries(this.concretizedDict)) {
+    for (const [key, value] of this.object_entries.call(null, this.concretizedDict)) {
       if (value[0] === f) {
         concretizationStrategy = value[1];
         break;
